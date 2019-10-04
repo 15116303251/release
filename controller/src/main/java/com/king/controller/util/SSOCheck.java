5ccadfe4a6e4620a44230d602333d061e69581c9
shenwang <15116303251@163.com>
2019-10-04 19:04:41 +0800

    删除同域cookie 的测试

--- controller/src/main/java/com/king/controller/util/SSOCheck.java
+++ controller/src/main/java/com/king/controller/util/SSOCheck.java
@@ -5,15 +5,14 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class SSOCheck {
 
 
     public static boolean CheckCookie(){
 
-        ServletRequestAttributes servletRequestAttributes= (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request=servletRequestAttributes.getRequest();
-
+        HttpServletRequest request=getRequest();
         Cookie[] cookies=request.getCookies();
 
         if(cookies!=null){
@@ -24,5 +23,23 @@ public class SSOCheck {
             }
         }
         return false;
+    }
+
+    public static boolean checkCookie(String cookieName,String cookieValue){
+        if("ssocookie".equalsIgnoreCase(cookieName) && "sso".equalsIgnoreCase(cookieValue)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static HttpServletResponse getResponse(){
+        ServletRequestAttributes servletRequestAttributes= (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return servletRequestAttributes.getResponse();
+    }
+
+    public static HttpServletRequest getRequest(){
+        ServletRequestAttributes servletRequestAttributes= (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        return servletRequestAttributes.getRequest();
     }
 }
